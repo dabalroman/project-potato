97e249e6f1b0fd8be6ad7f404ce8a9f07c2a458b
Roman Dąbal <dabalroman@gmail.com>
2021-05-12 22:55:12 +0200

    Readonly; Api/Auth JSDoc

--- front/src/Model/Auth.js
+++ front/src/Model/Auth.js
@@ -1,23 +1,42 @@
 import Api from '@/api/Api';
 import ApiUrls from '@/api/ApiUrls';
+import readonly from '@/utils/readonly';
 
 export default class Auth {
+  @readonly
+  LOCAL_STORAGE_TOKEN = 'token';
+  @readonly
+  LOCAL_STORAGE_TOKEN_EXPIRATION_DATE = 'tokenExpirationDate';
+
   loggedIn = false;
   token = null;
   tokenExpirationDate = null;
 
+  /**
+   * Create Auth object
+   * Autologin if valid JWT token is present in localStorage
+   */
   constructor () {
     this.token = localStorage.getItem('token');
 
     if (this.token) {
-      this.tokenExpirationDate = Date.parse(localStorage.getItem('tokenExpirationDate'));
+      this.tokenExpirationDate = Date.parse(localStorage.getItem(this.LOCAL_STORAGE_TOKEN_EXPIRATION_DATE));
 
       if (new Date() < this.tokenExpirationDate) {
         this.loggedIn = true;
+      } else {
+        localStorage.removeItem(this.LOCAL_STORAGE_TOKEN);
+        localStorage.removeItem(this.LOCAL_STORAGE_TOKEN_EXPIRATION_DATE);
       }
     }
   }
 
+  /**
+   * Login to api (retrieve JWT token)
+   * Token is stored in localStorage
+   * @param email
+   * @param password
+   */
   login (email, password) {
     let data = {
       email: email,
@@ -27,17 +46,22 @@ export default class Auth {
     let that = this;
     Api.post(function (data) {
       let tokenExpirationDate = new Date();
-      tokenExpirationDate.setTime(tokenExpirationDate.getTime() + data.expires_in * 1000);
+      tokenExpirationDate.setTime(tokenExpirationDate.getTime() + data['expires_in'] * 1000);
 
-      localStorage.setItem('token', data.access_token);
-      localStorage.setItem('tokenExpirationDate', tokenExpirationDate.toUTCString());
+      localStorage.setItem(that.LOCAL_STORAGE_TOKEN, data['access_token']);
+      localStorage.setItem(that.LOCAL_STORAGE_TOKEN_EXPIRATION_DATE, tokenExpirationDate.toUTCString());
 
       that.loggedIn = true;
       that.tokenExpirationDate = tokenExpirationDate;
     }, ApiUrls.auth.base + ApiUrls.auth.login, data);
   }
 
-  static getToken () {
+  /**
+   * Get JWT token as bearer string
+   * Returns empty string if token not found
+   * @return string
+   */
+  static getBearerToken () {
     let token = localStorage.getItem('token');
     return (token !== null) ? 'bearer ' + token : '';
   }
