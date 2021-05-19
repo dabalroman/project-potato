b688154c6186a9c0769674d513b1eefeacf4b686
Peterka15 <pklosek@onet.pl>
2021-05-19 21:50:36 +0200

    Item model added

--- front/src/api/ApiUrls.js
+++ front/src/api/ApiUrls.js
@@ -1,5 +1,6 @@
 export default {
   categories: 'http://localhost/project-potato/api/public/api/categories',
+  items: 'http://localhost/project-potato/api/public/api/items',
   auth: {
     base: 'http://localhost/project-potato/api/public/api/auth',
     register: '/register',
