3226285f2f1b4766b1e6eef2c388ee59abba94d6
Peterka15 <pklosek@onet.pl>
2021-05-21 23:58:14 +0200

    New view added

--- front/src/views/Main.vue
+++ front/src/views/Main.vue
@@ -1,18 +1,53 @@
 <template>
   <div id="main_view" class="fullscreen">
-    <div class="header">Header</div>
+    <div class="header">.</div>
     <b-container fluid="">
       <b-row>
+
         <b-col cols="8" class="column">
-          LISTA
+          <div v-for="category in categories" :key="category.id">
+            <div class="title">
+              <h3> {{ category.name }} </h3>
+            </div>
+            <!-- Tablica obiektów, domyślnie [], sprawdzanie czy długość != 0 -->
+
+            <div>
+
+            </div>
+
+          </div>
+
+          <div class="title">
+            <h3> LISTA </h3>
+          </div>
+
           <!-- Tablica obiektów, domyślnie [], sprawdzanie czy długość != 0 -->
-          <div v-if="categories.length" class="border border-2 border-primary">
+          <div v-if="categories.length" class="list">
+
             <div v-for="category in categories" :key="category.id">
               {{ category.name }}
             </div>
           </div>
         </b-col>
-        <b-col cols="4" class="column">
+
+
+        <!--        <b-col cols="4" class="column">-->
+        <!--          <div class="title">-->
+        <!--            <h3> LISTA </h3>-->
+        <!--          </div>-->
+
+        <!--          &lt;!&ndash; Tablica obiektów, domyślnie [], sprawdzanie czy długość != 0 &ndash;&gt;-->
+        <!--          <div v-if="categories.length" class="list">-->
+
+        <!--            <div v-for="category in categories" :key="category.id">-->
+        <!--              {{ category.name }}-->
+        <!--            </div>-->
+
+
+        <!--          </div>-->
+        <!--        </b-col>-->
+
+        <b-col cols="4" class="header">
           SZCZEGÓŁY
           <!-- Pojedynczy obiekt, domyślnie null, sprawdzanie czy obiekt istnieje i czy jest gotowy -->
           <div v-if="category && category.ready" class="border border-2 border-primary">
@@ -21,6 +56,7 @@
             </div>
           </div>
         </b-col>
+
       </b-row>
     </b-container>
   </div>
@@ -30,19 +66,22 @@
 import './../style/style.css';
 import Category from '@/Model/Category';
 
+
 export default {
   name: 'Main',
 
-  data () {
+  data() {
     return {
       categories: [],
       category: null
     };
   },
 
-  mounted () {
+  mounted() {
     this.categories = Category.getAll();
-    this.category = Category.getById(5);
+    //this.category = Category.create('BANANY');
+    //this.category = Item.create('Cebule',10, 20,Item.STATE_OK, "Cebula +10 do ataku", 1,1);
+    //this.category.save();
   }
 };
 </script>
