9d6fe035e594195a2b09be7a9b4de6fe76928196
Peterka15 <pklosek@onet.pl>
2021-05-23 21:25:19 +0200

    second column added + example data; sorting in first column added;

--- front/src/views/Main.vue
+++ front/src/views/Main.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="main_view" class="fullscreen">
-    <div class="header">.</div>
+    <div class="header"><h4>POTATO CRM</h4></div>
     <b-container fluid="">
       <b-row>
 
@@ -12,49 +12,34 @@
             <!-- Tablica obiektów, domyślnie [], sprawdzanie czy długość != 0 -->
 
             <div>
+              <!--                 <b-icon icon="exclamation-circle" ></b-icon>-->
+              <b-table
+
+                  :items="bla"
+                  :fields="fields"
+                  :sort-by.sync="sortBy"
+                  :sort-desc.sync="sortDesc"
+                  sort-icon-left
+                  responsive="sm"
+                  hover borderless="borderless"></b-table>
 
             </div>
 
           </div>
 
-          <div class="title">
-            <h3> LISTA </h3>
-          </div>
 
-          <!-- Tablica obiektów, domyślnie [], sprawdzanie czy długość != 0 -->
-          <div v-if="categories.length" class="list">
-
-            <div v-for="category in categories" :key="category.id">
-              {{ category.name }}
-            </div>
-          </div>
         </b-col>
 
 
-        <!--        <b-col cols="4" class="column">-->
-        <!--          <div class="title">-->
-        <!--            <h3> LISTA </h3>-->
-        <!--          </div>-->
-
-        <!--          &lt;!&ndash; Tablica obiektów, domyślnie [], sprawdzanie czy długość != 0 &ndash;&gt;-->
-        <!--          <div v-if="categories.length" class="list">-->
-
-        <!--            <div v-for="category in categories" :key="category.id">-->
-        <!--              {{ category.name }}-->
-        <!--            </div>-->
-
-
-        <!--          </div>-->
-        <!--        </b-col>-->
-
-        <b-col cols="4" class="header">
-          SZCZEGÓŁY
-          <!-- Pojedynczy obiekt, domyślnie null, sprawdzanie czy obiekt istnieje i czy jest gotowy -->
-          <div v-if="category && category.ready" class="border border-2 border-primary">
-            <div>
-              {{ category.name }}
-            </div>
+        <b-col cols="4" class="detail">
+          <div v-for="desc in desc" :key="desc.age">
+            <h4>{{ desc.first_name }}</h4>
           </div>
+          <!-- Pojedynczy obiekt, domyślnie null, sprawdzanie czy obiekt istnieje i czy jest gotowy -->
+          <div>
+            <b-table stacked :items="desc" borderless="borderless"></b-table>
+          </div>
+
         </b-col>
 
       </b-row>
@@ -65,6 +50,7 @@
 <script>
 import './../style/style.css';
 import Category from '@/Model/Category';
+import Item from "@/Model/Item";
 
 
 export default {
@@ -73,12 +59,27 @@ export default {
   data() {
     return {
       categories: [],
-      category: null
+      category: null,
+      items: [],
+      sortBy: 'age',
+      sortDesc: false,
+      bla: [{age: 40, first_name: 'Dickerson', last_name: 'Macdonald'},
+        {age: 21, first_name: 'Larsen', last_name: 'Shaw'},
+        {age: 89, first_name: 'Geneva', last_name: 'Wilson'},
+        {age: 38, first_name: 'Jami', last_name: 'Carney'}],
+      fields: [
+        {key: 'last_name', sortable: true},
+        {key: 'first_name', sortable: true},
+        {key: 'age', sortable: true},
+      ],
+      desc: [{age: 40, first_name: 'Dickerson', last_name: 'Macdonald'}],
+      item: null
     };
   },
 
   mounted() {
     this.categories = Category.getAll();
+    this.items = Item.getAll();
     //this.category = Category.create('BANANY');
     //this.category = Item.create('Cebule',10, 20,Item.STATE_OK, "Cebula +10 do ataku", 1,1);
     //this.category.save();
