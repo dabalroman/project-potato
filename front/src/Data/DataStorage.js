4e2a183168fedaa55c22c973c25d8a4880019927
Roman Dąbal <dabalroman@gmail.com>
2021-05-29 22:17:16 +0200

    Add users to dataStorage; Reindent file;

--- front/src/Data/DataStorage.js
+++ front/src/Data/DataStorage.js
@@ -1,95 +1,115 @@
 import DataContainer from '@/Data/DataContainer';
 
 export default class DataStorage {
-  /** @type {DataContainer} */
-  items;
-  /** @type {DataContainer} */
-  sources;
-  /** @type {DataContainer} */
-  users;
-  /** @type {DataContainer} */
-  categories;
+    /** @type {DataContainer} */
+    items;
+    /** @type {DataContainer} */
+    sources;
+    /** @type {DataContainer} */
+    users;
+    /** @type {DataContainer} */
+    categories;
 
-  /** @type {Boolean} */
-  ready = false;
+    /** @type {Boolean} */
+    ready = false;
 
-  constructor () {
-    this.items = new DataContainer(DataContainer.TYPE_ITEM);
-    this.sources = new DataContainer(DataContainer.TYPE_SOURCE);
-    this.categories = new DataContainer(DataContainer.TYPE_CATEGORY);
-  }
-
-  loadData () {
-    this.items.load(this.onDataAvailable.bind(this));
-    this.sources.load(this.onDataAvailable.bind(this));
-    this.categories.load(this.onDataAvailable.bind(this));
-  }
-
-  onDataAvailable () {
-    this.ready = this.isReady();
-
-    if (this.ready) {
-      console.log('Data storage ready.');
+    constructor () {
+        this.items = new DataContainer(DataContainer.TYPE_ITEM);
+        this.sources = new DataContainer(DataContainer.TYPE_SOURCE);
+        this.categories = new DataContainer(DataContainer.TYPE_CATEGORY);
+        this.users = new DataContainer(DataContainer.TYPE_USER);
     }
-  }
 
-  /**
-   * @return {boolean}
-   */
-  isReady () {
-    return this.items.ready && this.sources.ready && this.categories.ready;
-  }
+    loadData () {
+        this.items.load(this.onDataAvailable.bind(this));
+        this.sources.load(this.onDataAvailable.bind(this));
+        this.categories.load(this.onDataAvailable.bind(this));
+        this.users.load(this.onDataAvailable.bind(this));
+    }
 
-  /**
-   * @return {?Category[]}
-   */
-  getCategories () {
-    return this.categories.data;
-  }
+    onDataAvailable () {
+        this.ready = this.isReady();
 
-  /**
-   * @return {?Source[]}
-   */
-  getSources () {
-    return this.sources.data;
-  }
+        if (this.ready) {
+            console.log('Data storage ready.');
+        }
+    }
 
-  /**
-   * @return {?Item[]}
-   */
-  getItems () {
-    return this.items.data;
-  }
+    /**
+     * @return {boolean}
+     */
+    isReady () {
+        return this.items.ready
+            && this.sources.ready
+            && this.categories.ready
+            && this.users.ready;
+    }
 
-  /**
-   * @param {Category} category
-   * @return {Item[]}
-   */
-  getArrayOfItemsFromCategory (category) {
-    return this.items.data.filter(item => category.items.includes(item.id));
-  }
+    /**
+     * @return {?Category[]}
+     */
+    getCategories () {
+        return this.categories.data;
+    }
 
-  /**
-   * @param {number} id
-   * @return {Item|undefined}
-   */
-  getItem (id) {
-    return this.items.data.find(item => item.id === id);
-  }
+    /**
+     * @return {?Source[]}
+     */
+    getSources () {
+        return this.sources.data;
+    }
 
-  /**
-   * @param {Item} item
-   * @return {Source|undefined}
-   */
-  getSourceForItem (item) {
-    return this.sources.data.find(source => source.id === item.source);
-  }
+    /**
+     * @return {?Item[]}
+     */
+    getItems () {
+        return this.items.data;
+    }
 
-  /**
-   * @param {Item} item
-   * @return {Source|undefined}
-   */
-  getCategoryForItem (item) {
-    return this.categories.data.find(category => category.id === item.category);
-  }
+    /**
+     * @return {?User[]}
+     */
+    getUsers () {
+        return this.users.data;
+    }
+
+    /**
+     * @param {Category} category
+     * @return {Item[]}
+     */
+    getArrayOfItemsFromCategory (category) {
+        return this.items.data.filter(item => category.items.includes(item.id));
+    }
+
+    /**
+     * @param {number} id
+     * @return {Item|undefined}
+     */
+    getItem (id) {
+        return this.items.data.find(item => item.id === id);
+    }
+
+    /**
+     * @param {number} id
+     * @return {User|undefined}
+     */
+    getUser (id) {
+        return this.users.data.find(user => user.id === id);
+    }
+
+    /**
+     * @param {Item} item
+     * @return {Source|undefined}
+     */
+    getSourceForItem (item) {
+        return this.sources.data.find(source => source.id === item.source);
+    }
+
+    /**
+     * @param {Item} item
+     * @return {Source|undefined}
+     */
+    getCategoryForItem (item) {
+        return this.categories.data.find(category => category.id === item.category);
+    }
 }
